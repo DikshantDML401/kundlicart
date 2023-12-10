362c6ef4b476f6b0ec99129d751b1e1405791eac
DikshantDML401 <dikshant.choudhary@digimantra.com>
2023-12-10 21:42:26 +0530

    working on top tab navigation

--- src/screens/topNavigationScreens/home/index.tsx
+++ src/screens/topNavigationScreens/home/index.tsx
@@ -1,11 +1,27 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {homeBanner, kundliLogo} from '../../../utils/images';
 import {black} from '../../../styles/colors';
+import {font16Px} from '../../../utils/typography';
 
 const Homepage = () => {
   return (
     <View>
-      <Text style={styles.heading}>Homepage</Text>
+      <Image source={homeBanner} style={styles.homeBanner} />
+      <View style={styles.wrapper}>
+        <View style={styles.kundliWrapper}>
+          <Image source={kundliLogo} style={styles.kundliLogo} />
+          <Text style={styles.kundliText}>Kundli</Text>
+        </View>
+        <View style={styles.kundliWrapper}>
+          <Image source={kundliLogo} style={styles.kundliLogo} />
+          <Text style={styles.kundliText}>Kundli</Text>
+        </View>
+        <View style={styles.kundliWrapper}>
+          <Image source={kundliLogo} style={styles.kundliLogo} />
+          <Text style={styles.kundliText}>Kundli</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -13,7 +29,9 @@ const Homepage = () => {
 export default Homepage;
 
 const styles = StyleSheet.create({
-  heading: {
-    color: black,
-  },
+  homeBanner: {width: 'auto', height: 80},
+  wrapper:{flexDirection: 'row',gap: 1,alignItems: 'center', justifyContent: 'center'},
+  kundliWrapper: {justifyContent: 'center', alignItems: 'center',padding: 10, width:130, borderBottomColor: black, borderWidth: 2},
+  kundliLogo: {width: 40, height: 40},
+  kundliText: {color: black, fontSize: font16Px, fontWeight: '600', paddingTop: 10},
 });
--- src/utils/images.tsx
+++ src/utils/images.tsx
@@ -1,3 +1,7 @@
 const authHeaderLogo = require('../assets/images/narayana.png');
+const homeBanner = require('../assets/images/banner.jpeg');
+const kundliLogo = require('../assets/images/kundli.jpeg')
 
 export {authHeaderLogo};
+export {homeBanner};
+export {kundliLogo};
