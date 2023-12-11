1280e74d82334620e4fdcf981331f459b55257bb
dml-vipul <vipul.sharma@digimantra.com>
2023-12-11 10:31:10 +0530

    completed drawer navigation routes

---
+++ src/screens/AstroRegistration/index.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const AstroRegistration = () => {
+  return (
+    <View>
+      <Text>AstroRegistration</Text>
+    </View>
+  );
+};
+
+export default AstroRegistration;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/aboutUs/index.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default AboutUs;
+
+const styles = StyleSheet.create({});
